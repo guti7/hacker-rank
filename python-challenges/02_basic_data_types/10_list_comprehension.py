343c008830b65bc6d2d57ce6bfb7d31ed4917952
José Gutiérrez <guti7@users.noreply.github.com>
2017-02-01 12:24:53 -0800

    complete ex. 10 using list comprehensions.

--- python-challenges/02_basic_data_types/10_list_comprehension.py
+++
@@ -1,6 +0,0 @@
-# Basic Data Types in python: List Comprehensions (Hacker Rank) 
-
-# You are given three integers X, Y and Z representing the dimensions of
-# a cuboid along with an integer N.
-# Print all possible coordinates given by (i, j, k) on a 3D grid
-# where the sum of i + j + k is not equal to N.
---
+++ python-challenges/02_basic_data_types/10_list_comprehensions.py
@@ -0,0 +1,21 @@
+# Basic Data Types: List Comprehensions (Hacker Rank)
+
+# You are given three integers X, Y and Z representing the maximum dimensions
+# of a cuboid along with an integer N.
+# Print all possible coordinates given by (i, j, k) on a 3D grid
+# where the sum of i + j + k is not equal to N.
+import sys
+
+def calculate_cuboid_coordinates(x, y, z, n):
+    # how to break up list comprehensions?
+    return [[a, b, c] for a in range(x + 1) for b in range(y + 1) for c in range(z + 1) if a + b + c != n]
+
+if __name__ == '__main__':
+    # x = int(raw_input().strip())  # maybe use loop?
+    # y = int(raw_input().strip())
+    # z = int(raw_input().strip())
+    # n = int(raw_input().strip())
+    input_count = 4
+    x, y, z, n = (int(raw_input().strip()) for _ in range(input_count))
+
+    print calculate_cuboid_coordinates(x, y, z, n)
