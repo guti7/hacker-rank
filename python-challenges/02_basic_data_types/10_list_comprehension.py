88a2556c487a576f3046fee922984f2cbacf0477
José Gutiérrez <guti7@users.noreply.github.com>
2017-02-01 11:33:20 -0800

    change directory naming.

---
+++ python-challenges/02_basic_data_types/10_list_comprehension.py
@@ -0,0 +1,6 @@
+# Basic Data Types in python: List Comprehensions (Hacker Rank) 
+
+# You are given three integers X, Y and Z representing the dimensions of
+# a cuboid along with an integer N.
+# Print all possible coordinates given by (i, j, k) on a 3D grid
+# where the sum of i + j + k is not equal to N.
