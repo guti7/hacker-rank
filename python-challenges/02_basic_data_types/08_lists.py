ad8eaceb3707ec2aeb477e7a34e80247c2adacf7
José Gutiérrez <guti7@users.noreply.github.com>
2017-02-06 18:08:32 -0800

    correct indentation format in while loop.

--- python-challenges/02_basic_data_types/08_lists.py
+++ python-challenges/02_basic_data_types/08_lists.py
@@ -8,22 +8,22 @@ if __name__ == '__main__':
     N = int(raw_input())
     list = []
 
-while N > 0:
-    N -= 1
-    #print "%r" % N
-    data = raw_input().split()
-    if data[0] == "print":
-        print list
-    elif data[0] == "insert":
-        #print "insert(%f, %d)" % (float(data[1]), float(data[2]))
-        list.insert(int(data[1]), int(data[2]))
-    elif data[0] == "remove":
-        list.remove(int(data[1]))
-    elif data[0] == "append":
-        list.append(int(data[1]))
-    elif data[0] == "sort":
-        list.sort()
-    elif data[0] == "pop":
-        list.pop()
-    elif data[0] == "reverse":
-        list.reverse()
+    while N > 0:
+        N -= 1
+        #print "%r" % N
+        data = raw_input().split()
+        if data[0] == "print":
+            print list
+        elif data[0] == "insert":
+            #print "insert(%f, %d)" % (float(data[1]), float(data[2]))
+            list.insert(int(data[1]), int(data[2]))
+        elif data[0] == "remove":
+            list.remove(int(data[1]))
+        elif data[0] == "append":
+            list.append(int(data[1]))
+        elif data[0] == "sort":
+            list.sort()
+        elif data[0] == "pop":
+            list.pop()
+        elif data[0] == "reverse":
+            list.reverse()
